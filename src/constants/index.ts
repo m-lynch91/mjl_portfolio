a982b9df725d6b27715bacb8578f87c39834265b
Mike Lynch <micheal.j.lynch@gmail.com>
2025-04-25 10:15:41 -0400

    initial commit of portfolio

---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+import { Word }  from "../types/Word"
+
+export const words: Word[] = [
+    { text: 'Ideas', imgPath: '/images/ideas.svg' },
+    { text: 'Concepts', imgPath: '/images/concepts.svg' },
+];

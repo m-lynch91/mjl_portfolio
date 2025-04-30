dafe95aacf11b2b6fd6e4655dc977f6ca54eda07
Mike Lynch <micheal.j.lynch@gmail.com>
2025-04-30 11:50:39 -0400

    started changing the template to match my actual experience, further personalization required

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -160,54 +160,52 @@ const expCards: ExperienceCard[] = [
 	{
 		review: "Adrian brought creativity and technical expertise to the team, significantly improving our frontend performance. His work has been invaluable in delivering faster experiences.",
 		imgPath: "/images/exp1.png",
-		logoPath: "/images/logo1.png",
-		title: "Frontend Developer",
-		date: "January 2023 - Present",
+		logoPath: "/images/rcmp-logo1.png",
+		title: "Programmer Analyst",
+		date: "December 2024 - Present",
 		responsibilities: [
-			"Developed and maintained user-facing features for the Hostinger website.",
-			"Collaborated closely with UI/UX designers to ensure seamless user experiences.",
-			"Optimized web applications for maximum speed and scalability.",
+			"Designed and executed Oracle database schema modifications using Data Definition Language (DDL) changes, ensuring backward compatibility and supporting new feature deployments.",
+			"Refactored Java Server Faces markup and Java EE enterprise beans, resolving critical issues that previously blocked data persistence and validation error visibility.",
+			"Worked with stakeholders and technology partners to ensure solutions met technical direction, requirements, performance, and deadline expectations.",
+		],
+	},
+	{
+		review: "Adrian brought creativity and technical expertise to the team, significantly improving our frontend performance. His work has been invaluable in delivering faster experiences.",
+		imgPath: "/images/exp1.png",
+		logoPath: "/images/rcmp-logo1.png",
+		title: "Junior Programmer Analyst",
+		date: "September 2023 - December 2024",
+		responsibilities: [
+			"Collaborated with Technical Advisor to design and implement a REST API to modernize Missing Children Persons and Unidentified Remains application using Spring Boot.",
+			"Enhanced the RCMP School Action For Emergencies application’s offline reporting capabilities, increasing the number of schools generating reports.",
+			"Optimized Java EE bean by integrating a virtualizer, reducing Jasper Report object memory consumption and enabling processing of larger report file sizes.",
 		],
 	},
 	{
 		review: "Adrian’s contributions to Docker's web applications have been outstanding. He approaches challenges with a problem-solving mindset.",
 		imgPath: "/images/exp2.png",
-		logoPath: "/images/logo2.png",
-		title: "Full Stack Developer",
-		date: "June 2020 - December 2023",
+		logoPath: "/images/buckland-logo1.png",
+		title: "Junior Developer",
+		date: "January 2023 - April 2023",
 		responsibilities: [
-			"Led the development of Docker's web applications, focusing on scalability.",
-			"Worked with backend engineers to integrate APIs seamlessly with the frontend.",
-			"Contributed to open-source projects that were used with the Docker ecosystem.",
+			"Built multiple UI components for a new company web application using Blazor and Radzen.",
+			"Implemented REST API endpoints in C# / .NET, facilitating seamless communication between the front-end and back-end services.",
+			"Wrote SQL scripts for data processing, resolving priority JIRA tickets and improving Business Analyst workflow.",
+			"Delivered code updates through Azure DevOps, incorporating peer feedback to maintain high code quality and adherence to best practices."
 		],
 	},
-	{
-		review: "Adrian’s work on Appwrite’s mobile app brought a high level of quality and efficiency. He delivered solutions that enhanced our mobile experience & meet our product goals.",
-		imgPath: "/images/exp3.png",
-		logoPath: "/images/logo3.png",
-		title: "React Native Developer",
-		date: "March 2019 - May 2020",
-		responsibilities: [
-			"Built cross-platform mobile apps using React Native, integrating with Appwrite's backend services.",
-			"Improved app performance and user experience through code optimization and testing.",
-			"Coordinated with the product team to implement features based on feedback.",
-		],
-	},
+
 ];
 
 const expLogos: Image[] = [
 	{
-		name: "logo1",
-		imgPath: "/images/logo1.png",
+		name: "rcmpLogo",
+		imgPath: "/images/rcmp-logo.png",
 	},
 	{
-		name: "logo2",
-		imgPath: "/images/logo2.png",
-	},
-	{
-		name: "logo3",
-		imgPath: "/images/logo3.png",
-	},
+		name: "bucklandLogo",
+		imgPath: "/images/buckland-logo.png",
+	}
 ];
 
 const testimonials: Testimonial[] = [

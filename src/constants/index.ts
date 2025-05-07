ddea24e3977654dc9947960e1c6bf64df9f09444
Mike Lynch <micheal.j.lynch@gmail.com>
2025-05-07 11:00:46 -0400

    finishing off contact me section

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -196,28 +196,7 @@ const testimonials: Testimonial[] = [
 		review:
 			"Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
 		imgPath: "/images/client2.png",
-	},
-	{
-		name: "Marvin McKinney",
-		mentions: "@marvinmckinney",
-		review:
-			"Adrian was a pleasure to work with. He turned our outdated website into a fresh, intuitive platform that’s both modern and easy to navigate. Fantastic work overall.",
-		imgPath: "/images/client5.png",
-	},
-	{
-		name: "Floyd Miles",
-		mentions: "@floydmiles",
-		review:
-			"Adrian’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional!",
-		imgPath: "/images/client4.png",
-	},
-	{
-		name: "Albert Flores",
-		mentions: "@albertflores",
-		review:
-			"Adrian was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend and backend dev are top-notch.",
-		imgPath: "/images/client6.png",
-	},
+	}
 ];
 
 const socialImgs: Image[] = [

2f13432a6897f57d9eeeb18bbf0c5fdc705049e0
Mike Lynch <micheal.j.lynch@gmail.com>
2025-05-08 18:11:56 -0400

    styling changes and modified tech stack appearance

--- src/sections/TechStack.tsx
+++ src/sections/TechStack.tsx
@@ -5,25 +5,30 @@ const TechStack = () => {
   return (
     <div id="skills" className="flex-center section-padding">
       <div className="w-full h-full md:px-10 px-5">
-        <TitleHeader
-          title="My Preferred Tech Stack"
-          sub="The Skills I Bring to the Table"
-        />
+        <TitleHeader title="My Tech Stack" sub="Skills" />
 
-        <div className="tech-grid">
-          {techStackImgs.map((icon) => (
-            <div
-              key={icon.name}
-              className="card-border tech-card overflow-hidden group xl:rounded-full rounded-sm"
-            >
-              <div className="tech-card-content">
-                <div className="tech-icon-wrapper">
-                  <img src={icon.imgPath} />
+        <div className="tech-stack">
+          <div className="tech-stack-grid">
+            {techStackImgs.map((icon) => (
+              <div
+                key={icon.name}
+                className="card-border rounded-xl p-2 flex flex-col gap-4"
+              >
+                <div className="p-6 shadow-white-50 shadow-lg rounded-xl hover:scale-105 ease-in duration-50">
+                  <div className="grid grid-cols-2 gap-4 justify-center items-center">
+                    <div className="m-auto">
+                      <img src={icon.imgPath} width={64} height={64} />
+                    </div>
+                    <div className="flex flex-col items-center justify-center">
+                      <p className="m-auto text-white-50 font-semibold">
+                        {icon.name}
+                      </p>
+                    </div>
+                  </div>
                 </div>
-                <p>{icon.name}</p>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </div>
